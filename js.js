a4446acead21a39c8fa2797a8a3bb0ca787f6fba
mikestepanov <mikestepanov777@gmail.com>
2017-12-10 20:30:53 -0800

    added checks

--- js.js
+++ js.js
@@ -1,24 +1,71 @@
 function createBlocks() {
   for (var i = 0; i < 9; i++) {
     var div = document.createElement('div');
-    div.setAttribute('id', i);
+    div.setAttribute('id', `cluster${i}`);
     $('#sudoku').append(div);
   }
 }
 
-var arr = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
 
 createBlocks();
+
+function getCorrectParentId(i, j) {
+  var arr = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+  var num = i * 9 + j;
+  var line = arr[Math.floor(num / 27)]
+  num = num % 9;
+  return line[Math.floor(num / 3)];
+}
 
 for (var i = 0; i < 9; i++) {
   for (var j = 0; j < 9; j++) {
     var span = document.createElement('span');
-    var num = i * 9 + j;
     span.classList.add('v' + i);
     span.classList.add('h' + j);
-    var line = arr[Math.floor(num / 27)]
-    num = num % 9;
-    var id = line[Math.floor(num / 3)];
-    $(`#${id}`).append(span);
+    var id = getCorrectParentId(i, j);
+    span.innerHTML = `<p>-</p>`;
+    $(`#cluster${id}`).append(span);
   }
+}
+
+function checkCluster(n) {
+  var arr = document.querySelectorAll(`#cluster${n} span p`);
+  var holdArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if(holdArr.includes(arr[i].innerHTML)) {
+      return false;
+    }
+    if(arr[i].innerHTML !== '-') {
+      holdArr.push(arr[i].innerHTML);
+    }
+  }
+  return true;
+}
+
+function checkRow(n) {
+  var arr = document.querySelectorAll(`.h${n} p`);
+  var holdArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if(holdArr.includes(arr[i].innerHTML)) {
+      return false;
+    }
+    if(arr[i].innerHTML !== '-') {
+      holdArr.push(arr[i].innerHTML);
+    }
+  }
+  return true;
+}
+
+function checkCol(n) {
+  var arr = document.querySelectorAll(`.v${n} p`);
+  var holdArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if(holdArr.includes(arr[i].innerHTML)) {
+      return false;
+    }
+    if(arr[i].innerHTML !== '-') {
+      holdArr.push(arr[i].innerHTML);
+    }
+  }
+  return true;
 }
